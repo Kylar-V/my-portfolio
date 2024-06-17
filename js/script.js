e25fd1fc81222c155c0296225285d4d67e69d191
Kai <kylar_valentine@hotmail.co.uk>
2024-06-17 18:50:14 +0200

    making each section responsive with media queries

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("#main-nav a");
+  
+    navLinks.forEach(function(navLink) {
+      navLink.addEventListener("click", function(event) {
+        event.preventDefault();
+  
+        const targetId = navLink.getAttribute("href").substring(1);
+        const targetSection = document.getElementById(targetId);
+  
+        if (targetSection) {
+          targetSection.scrollIntoView({
+            behavior: "smooth"
+          });
+        }
+      });
+    });
+  });
+  
